2f122882df1bdad5d4324862442ce77ccec3f576
alejandro-velasco <alejandro.velasco1423@gmail.com>
2021-12-31 09:30:30 -0600

    adding GitRepos test case

--- gitFlow/tests.py
+++ gitFlow/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import GitRepos
 
-# Create your tests here.
+class GitReposTestCase(TestCase):
+    def test_git_repo_created(self):
+        git_repo_obj = GitRepos.objects.create(
+            name='test-case-repo',
+            url='https://github.com/testcase/repo.git',
+            description='A Test Case Repository',
+            secure='True'
+        )
+        self.assertEqual(git_repo_obj.id, 1)
